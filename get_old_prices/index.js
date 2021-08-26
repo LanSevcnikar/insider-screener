37b0b9c8ce57d7a8a27e6afad76f1c9184c4bcc1
Lan Sevčnikar <sevcnikar.lan@gmail.com>
2021-08-26 16:59:51 +0200

    Small updates
    Did not do that much but some playing around.
    
    The biggest problem at the moment is how do I make this really effective for the user to retrive back. The slowest thing will proably be the simulation so it might make sense to make something that is tailored to doing that quickly.
    
    Will most likely therefore make a dictoanrz type thing pol pa v kolikor rabim sortiranje ali kaj podobnega bom spremenil

--- get_old_prices/index.js
+++ get_old_prices/index.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const Threader = require("./threader");
+const Timestamp = require("unix-timestamp")
 
 const threader = new Threader();
 
@@ -23,10 +24,35 @@ async function _main() {
   threader.run_threads(3);
 }
 
-for(let i = 0; i < 1; i++){
+for (let i = 0; i < 1; i++) {
+  const ticker = "AMZN";
+  const start = Timestamp.fromDate("2010-01-01");
+  const stop = Timestamp.fromDate("2021-08-25");
 
-  const url =  "https://query2.finance.yahoo.com/v8/finance/chart/SUI?formatted=true&crumb=hIGzkY.KjVk&lang=en-US&region=US&includeAdjustedClose=true&interval=1d&period1=1009843200&period2=1628640000&events=capitalGain%7Cdiv%7Csplit&useYfid=true&corsDomain=finance.yahoo.com"
-  fetch(url).then(res => res.json()).then(res => console.log(res.chart.result[0].timestamp[res.chart.result[0].timestamp.length-1]));
+  const url = `
+    https://query2.finance.yahoo.com/v8/finance/chart/${ticker}
+    ?formatted=true
+    &crumb=hIGzkY.KjVk
+    &lang=en-US
+    &region=US
+    &includeAdjustedClose=true
+    &interval=1d
+    &period1=${start}
+    &period2=${stop}
+    &events=capitalGain%7Cdiv%7Csplit
+    &useYfid=true
+    &corsDomain=finance.yahoo.com`
+    .split(' ').join('').split("\n").join("")
+
+  fetch(url).then(res => res.json()).then(res => {
+    console.log("Successful fetch")
+    res.chart.result[0].timestamp.forEach((date,  index) => {
+      console.log(
+        index,
+        Timestamp.toDate(date),
+        res.chart.result[0].indicators.adjclose[0].adjclose[index]
+      )
+    });
+  });
 
 }
-
